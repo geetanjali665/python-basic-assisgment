0105b5d629445ba0ee1fd16762fc509c5d619879
Geetanjali kumari <79591079+geetanjali665@users.noreply.github.com>
2023-07-19 15:10:47 +0530

    Add files via upload

---
+++ python_basic_assignment_1.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+"""1. In the below elements which of them are values or an expression? eg:- values can be
+integer or string and expressions will be mathematical operators.
+*
+"hello"
+-87.8
+-
+/
++
+6"""
+
+
+# In[2]:
+
+
+"""* - expression
+"hello "- value(string)
+-87.8 - value (float)
+-  - expression
+/ - expression
++ - expression
+6 - value"""
+
+
+# In[3]:
+
+
+"""What is the difference between string and variable?
+string is a data type in python where we store string variable . variable is basically a container where we store our data"""
+
+
+# In[4]:
+
+
+"""3. Describe three different data types.
+intger
+string
+float
+tuple
+list
+dictonary
+complex
+boolean"""
+
+
+# In[7]:
+
+
+"""4. What is an expression made up of? What do all expressions do?
+expresion is basically combination of values , variable, opearator and calls to function . interperator evaluates the expression 
+and give result"""
+
+
+# In[8]:
+
+
+""" 5. This assignment statements, like spam = 10. What is the difference between an
+expression and a statement?
+statement is basically used for creating varibales and displaying output. and varibales is creating output python interperter used 
+to run expreesion and give us output.
+"""
+
+
+# In[9]:
+
+
+"""6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1"""
+
+
+# In[10]:
+
+
+bacon = 22
+bacon + 1
+
+
+# In[11]:
+
+
+"spam" + "spamspam"
+
+
+# In[13]:
+
+
+"spam"*3
+
+
+# In[14]:
+
+
+"""8. Why is eggs a valid variable name while 100 is invalid?
+eggs is a valid variable name but when you will create variable name is 100 then interperator gets confused beczz 100 itsself a intger variable
+and it will give a error
+"""
+
+
+# In[15]:
+
+
+"""9. What three functions can be used to get the integer, floating-point number, or string
+version of a value?
+int
+decimal
+varchar
+
+"""
+
+
+# In[16]:
+
+
+"""10. Why does this expression cause an error? How can you fix it?
+"I have eaten" + 99 + "burritos"
+
+Here this expression give us error because we are trying to perform summation opertaion on integer and string but its not possible
+to fix this statement we have to change integer into string.
+
+"""
+
+
+# In[ ]:
+
+
+
+
